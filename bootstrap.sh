484182e86e8248ee0a625f8c3cf8af26ea564b58
mbarbier <mbarbier@smart-trade.net>
2014-08-22 11:14:59 +0200

    Add forwarding of 8100 and 35729, install sass, restart adb after boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,6 +28,4 @@ expect {
 }
 '
 
-sudo /home/vagrant/android-sdk-linux/platform-tools/adb kill-server
-sudo /home/vagrant/android-sdk-linux/platform-tools/adb start-server
-sudo /home/vagrant/android-sdk-linux/platform-tools/adb devices
+sudo gem install sass
